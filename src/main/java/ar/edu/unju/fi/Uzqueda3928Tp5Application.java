8bf9cd22a0c6f97bee27950172cfa29ef15e1e26
piola <piola@LAPTOP-OMDPGF5A>
2020-05-27 16:49:44 -0300

    Asunto: Inicializacion del Proyecto Responsable: Uzqueda Jose task01: Inicializacion del proyecto

---
+++ src/main/java/ar/edu/unju/fi/Uzqueda3928Tp5Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uzqueda3928Tp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uzqueda3928Tp5Application.class, args);
+	}
+
+}
